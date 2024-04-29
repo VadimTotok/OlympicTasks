2d1e2186e06fdf14a4405795f40af9d7a5a1f488
VadimTotok <112573798+VadimTotok@users.noreply.github.com>
2024-04-29 11:40:36 +0300

    update

--- year2023/src/Task2.java
+++ year2023/src/Task2.java
@@ -11,14 +11,12 @@ public class Task2 {
 
         int[] qArray = new int[m + n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             qArray[i + m] = q * (i + 1);
-        }
 
         int tm = 0;
-        for (int elem : qArray) {
+        for (int elem : qArray)
             tm = Math.max(tm, elem) + t;
-        }
 
         System.out.println(tm);
     }
