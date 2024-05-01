d8d55d1ad3fbeb87572dac5507e804d6af975411
VadimTotok <112573798+VadimTotok@users.noreply.github.com>
2024-05-01 19:26:43 +0300

    Update Task3.java

--- year2023/src/Task3.java
+++ year2023/src/Task3.java
@@ -7,25 +7,30 @@ public class Task3 {
         int n = scanner.nextInt();
         char[] s = scanner.next().toCharArray();
 
-        for (int l = 0; l < n; l++) {
-            for (int r = l; r < n; r++) {
-                char[] s1 = s.clone();
-                for (int i = l; i <= r; i++) {
-                    if (s1[i] == '1') {
-                        s1[i] = '0';
-                    } else {
-                        s1[i] = '1';
-                    }
-                }
+        int l = 0;
+        int r = n - 1;
 
-                if (isEqual(s1, s1[0], n)) {
-                    System.out.println((l + 1) + " " + (r + 1));
-                    System.exit(0);
+        while (l <= r) {
+            char[] sClone = s.clone();
+
+            for (int i = l; i <= r; i++) {
+                if (sClone[i] == '1') {
+                    sClone[i] = '0';
+                } else {
+                    sClone[i] = '1';
                 }
             }
+
+            if (isEqual(sClone, sClone[0], n)) {
+                System.out.println((l + 1) + " " + (r + 1));
+                return;
+            }
+
+            l++;
+            r--;
         }
 
-        System.out.println(-1);
+        System.out.println("-1");
     }
 
     private static boolean isEqual(char[] arr, char value, int n) {
